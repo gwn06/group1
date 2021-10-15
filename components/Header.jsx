d6edc9716224e77da6bd877e014f919b238eb10f
ReinetteTinekerr <reinettetinekerr@gmail.com>
2021-10-15 11:49:02 +0800

    fuck

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,14 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-gray-900 text-gray-100 w-full">
+    <header className="bg-pink-700 text-gray-100 w-full">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link href="/">
-            <a>
+        <Link href="/">
+          <a>
             <span className="ml-3 text-xl">Group 1 ka</span>
-            </a>
-          </Link>
+          </a>
+        </Link>
         <nav className="flex flex-wrap md:w-4/5 items-center justify-end text-base md:ml-auto">
           <Link href="/gg">
             <a className="mx-5 cursor-pointer uppercase hover:text-indigo-300">
